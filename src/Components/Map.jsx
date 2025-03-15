02057edfd0e3219596ce9175d35f8a26b17d002d
AngelinFrancis <angelin.francis98@gmail.com>
2025-03-16 01:03:41 +1100

    Partial commit

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,8 @@
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  Circle,
-  Polygon
-} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import SearchBar from "./SearchBar";
+import VotingBar from "./VotingBar";
 
 const MapComponent = () => {
   const markerIcon = new L.Icon({
@@ -17,7 +11,7 @@ const MapComponent = () => {
     iconAnchor: [16, 32],
     popupAnchor: [0, -32],
   });
-  
+
   const polygon = [
     [51.51, -0.12],
     [51.52, -0.14],
@@ -25,49 +19,38 @@ const MapComponent = () => {
   ];
 
   return (
-    <MapContainer
-      center={[-33.9173, 151.2313]}
-      zoom={13}
-      className="w-full h-full"
-    >
-      <SearchBar />
-      <TileLayer
-        //differnt tile layers can be used here, e.g.,:
-        //most common openstreetmap tiles
-        // url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-        // High resolution black and white map
-        // url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
-
-        // toner but lighter version
-        // url="https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png"
-
-        // also commom but prettier map
-        // url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
-
-        // water color map omg this is pretty cool
-        url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
-        attribution='&copy; <a href="https://stamen.com">Stamen Design</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      />
-      
-      <TileLayer url="https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.png" />
-      
-      <TileLayer url="https://tiles.stadiamaps.com/tiles/stamen_toner_lines/{z}/{x}/{y}{r}.png" opacity={0.1} />
-
-      <Marker position={[51.505, -0.09]} icon={markerIcon}>
-        <Popup>Hello, this is a custom marker!</Popup>
-      </Marker>
-
-      {/* <Circle
+    <div className="relative h-screen w-full">
+      <MapContainer
         center={[-33.9173, 151.2313]}
-        radius={500}
-        color="red"
-        fillColor="pink"
-      /> */}
+        zoom={13}
+        className="w-full h-full -z-0 "
+      >
+        
+        <SearchBar />
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
+          attribution='&copy; <a href="https://stamen.com">Stamen Design</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        />
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.png"
+         
+        />
+        <TileLayer
+          url="https://tiles.stadiamaps.com/tiles/stamen_toner_lines/{z}/{x}/{y}{r}.png"
+          opacity={0.1}
+        />
 
-      <Polygon positions={polygon} color="blue" />
-    </MapContainer>
+        <Marker position={[-33.9173, 151.2313]} icon={markerIcon}>
+          <Popup>Hello, this is a custom marker!</Popup>
+        </Marker>
+        <Marker position={[-33.9073, 151.2313]} icon={markerIcon}>
+          <Popup>Hello, this is a custom marker!</Popup>
+        </Marker>
+
+        <Polygon positions={polygon} color="blue" />
+      </MapContainer>
+    </div>
   );
-}
+};
 
 export default MapComponent;
--- src/Event.jsx
+++ src/Event.jsx
@@ -5,7 +5,7 @@ import Availability from "./Components/Availability/Availability.jsx";
 import Map from "./Components/Map.jsx";
 import VotingBar from "./Components/VotingBar.jsx";
 import NameBox from "./Components/NameBox.jsx";
-
+import NameInput from "./Components/NameInput.jsx";
 function Event() {
   const [name, setName] = useState(["Alice", "Bob", "Charlie"]);
 
@@ -36,15 +36,19 @@ function Event() {
   }, [uniqueLink]);
 
   return (
-    <>
-      <h1 className="text-4xl font-tilt text-black bg-amber-300 text-center p-4">
-        RendezWho
-      </h1>
+    <div className='flex flex-col w-full h-100%'>
+      <h1 className="flex text-2xl font-bold text-white bg-violet-500 p-2">RendezWho</h1>
+      <div className="flex flex-col ">
+
+      </div>
       {eventData ? (
-        <div className="mt-6 text-center">
-          <h2 className="text-purple-800 text-4xl font-bold">
-            Event Name: {eventData.name}
-          </h2>
+        <div className="flex flex-col p-3">
+          <h2 className="text-5xl ">{eventData.name}</h2>
+          <div className="flex flex-row gap-3 opacity-90">
+            {name.map((n, index) => (
+              <NameBox key={index} name={n} />
+            ))}
+          </div>
           {/* Can add more event details */}
         </div>
       ) : (
@@ -52,23 +56,34 @@ function Event() {
           Loading event details...
         </p>
       )}
-      <div className="min-h-screen flex gap-5 flex-col p-4 px-10 overflow-auto md:flex-row">
-        <div className="flex-row gap-2 flex-1  md:flex-col">
-          {name.map((n, index) => (
-            <NameBox key={index} name={n} />
-          ))}
+      <div className="min-h-screen flex gap-5 justify-center items-start flex-col p-4 px-10 overflow-auto md:flex-row">
+        <div className="flex-col gap-2 flex-1  ">
+          <div className="w-[150px] h-[30px] md:flex-row">
+            {/* <div className="w-full h-full flex mt-2 bg-gray-100 rounded-md items-center justify-left">
+              <input
+                className="w-full pl-4 rounded-md text-gray-700"
+                type="text"
+                placeholder="your name"
+              />
+              <button className="w-20 h-8 bg-[#4379F2] text-white rounded-md text-sm">
+                Add
+              </button>
+            </div> */}
+          </div>
         </div>
-
-        <div className=" flex flex-1 w-full md:w-1/3">
+        <div className=" flex flex-2 h-screen md:w-1/3">
           <Availability></Availability>
         </div>
-
-        <div className="flex flex-col items-center justify-center w-full md:w-2/3 gap-4">
-          <Map />
-          <VotingBar />
+        <div className="flex flex-3 flex-col items-center justify-center w-full gap-4">
+          <div className="w-full h-[500px] rounded-lg">
+            <Map />
+          </div>
+          {/* <div className="w-full h-[400px] ">
+            <VotingBar />
+          </div> */}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 

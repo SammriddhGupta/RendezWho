324d37f0927e4e2efd50d2975e20e61ce3640216
AngelinFrancis <angelin.francis98@gmail.com>
2025-03-15 23:27:12 +1100

    Added data

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,12 @@
 import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 import { useMap } from "react-leaflet";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "leaflet-geosearch/assets/css/leaflet.css";
 
+
 const SearchBar = () => {
+  const [ location, setLocation ] = useState({});
+
   const searchControl = new GeoSearchControl({
     provider: new OpenStreetMapProvider(),
     style: 'bar',
@@ -12,8 +15,22 @@ const SearchBar = () => {
 
   const map = useMap();
 
+  const searchEventHandler = (result) => {
+    const bounds = [];
+    bounds.push(result.location.bounds[0]);
+    bounds.push(result.location.bounds[1]);
+
+    setLocation({
+      name: result.location.label,
+      x: result.location.x,
+      y: result.location.y,
+      bounds: bounds
+    });
+  }
+
   useEffect(() => {
     map.addControl(searchControl);
+    map.on('geosearch/showlocation', searchEventHandler);
     return () => map.removeControl(searchControl);
   }, []);
 

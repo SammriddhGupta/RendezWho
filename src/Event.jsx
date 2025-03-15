3963e44624824418ba914232caf877cbccba469d
Charlene <z5538542@ad.unsw.edu.au>
2025-03-15 20:39:05 +1100

    chore: merge main

--- src/Event.jsx
+++ src/Event.jsx
@@ -5,7 +5,6 @@ import Day from "./Components/Availability/Day.jsx";
 import Map from "./Components/Map.jsx";
 import VotingBar from "./Components/VotingBar.jsx"
 import NameBox from "./Components/NameBox.jsx";
-import { useState } from "react";
 
 function Event() {
   const [name, setName] = useState(["Alice", "Bob", "Charlie"]);
@@ -42,6 +41,18 @@ function Event() {
       <h1 className="text-4xl font-tilt text-black bg-amber-300 text-center p-4">
         RendezWho
       </h1>
+      {eventData ? (
+        <div className="mt-6 text-center">
+          <h2 className="text-purple-800 text-4xl font-bold">
+            Event Name: {eventData.name}
+          </h2>
+          {/* Can add more event details */}
+        </div>
+      ) : (
+        <p className="text-gray-500 mt-4 text-center text-2xl font-bold">
+          Loading event details...
+        </p>
+      )}
       <div className="min-h-screen flex gap-5 flex-col p-4 px-10 overflow-auto md:flex-row">
         <div className="flex-row gap-2 flex-1  md:flex-col">
           {name.map((n, index) => (
@@ -53,11 +64,10 @@ function Event() {
           <Day />
         </div>
 
-          <div className="flex flex-col items-center justify-center w-full md:w-2/3 gap-4">
-            <Map />
-            <VotingBar />
-            </div>
-      
+        <div className="flex flex-col items-center justify-center w-full md:w-2/3 gap-4">
+          <Map />
+          <VotingBar />
+        </div>
       </div>
     </>
   );

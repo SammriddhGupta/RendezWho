0a88abc4d48e02942f83e10326b87c54d2ad6447
SammriddhGupta <sammriddhgupta@gmail.com>
2025-03-15 16:51:27 +1100

    fix-UI: allow components to occupy full page, map and days

--- src/Event.jsx
+++ src/Event.jsx
@@ -1,14 +1,26 @@
+import "./index.css";
 import Day from "./Components/Availability/Day.jsx";
 import Map from "./Components/Map.jsx";
 
-const Event = () => {
+function Event() {
   return (
-    <div className="flex items-center justify-center bg-gray-100 p-6 min-h-screen w-full">
-      <div className="flex flex-col md:flex-row justify-center items-center w-9/10 gap-5 md:gap-10 bg-white rounded-lg p-5">
-        <Day/>
-        <Map />
+    <>
+       <h1 className="text-4xl font-bold text-purple-600 text-center mb-6 mt-6">
+        RendezWho
+      </h1>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-6">
+     
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-6 w-full max-w-7xl">
+        <div className="w-full md:w-1/3">
+          <Day />
+        </div>
+
+        <div className="w-full md:w-2/3">
+          <Map />
+        </div>
       </div>
     </div>
+    </>
   );
 }
 

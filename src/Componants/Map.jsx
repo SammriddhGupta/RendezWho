b1730468cc280cc30bded79f91adbe834184e61c
AngelinFrancis <angelin.francis98@gmail.com>
2025-03-15 13:40:42 +1100

    Implemented GeoSearchControl

--- src/Componants/Map.jsx
+++ src/Componants/Map.jsx
@@ -4,39 +4,37 @@ import {
   Marker,
   Popup,
   Circle,
-  Polygon,
-  useMap
+  Polygon
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { OpenStreetMapProvider, GeoSearchControl } from "leaflet-geosearch";
-import "leaflet-geosearch/dist/geosearch.css";
 import L from "leaflet";
-import { useEffect } from "react";
+import SearchBar from "./SearchBar";
 
-export default function MapComponant() {
-    const markerIcon = new L.Icon({
-      iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
-      popupAnchor: [0, -32],
-    });
-   
-    const polygon = [
+export default function MapComponent() {
+  const markerIcon = new L.Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+  });
+  
+  const polygon = [
     [51.51, -0.12],
     [51.52, -0.14],
     [51.5, -0.15],
-    ];
-    
+  ];
+
   return (
     <MapContainer
       center={[-33.9173, 151.2313]}
       zoom={13}
       style={{ height: "500px", width: "100%" }}
     >
+      <SearchBar />
       <TileLayer
         //differnt tile layers can be used here, e.g.,:
         //most common openstreetmap tiles
-        // url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
 
         // High resolution black and white map
         // url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
@@ -48,8 +46,8 @@ export default function MapComponant() {
         // url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
 
         // water color map omg this is pretty cool
-        url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
-        attribution='&copy; <a href="https://stamen.com">Stamen Design</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        // url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
+        // attribution='&copy; <a href="https://stamen.com">Stamen Design</a> | <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
       
       <TileLayer url="https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.png" />
@@ -59,6 +57,7 @@ export default function MapComponant() {
       <Marker position={[51.505, -0.09]} icon={markerIcon}>
         <Popup>Hello, this is a custom marker!</Popup>
       </Marker>
+
       <Circle
         center={[-33.9173, 151.2313]}
         radius={500}

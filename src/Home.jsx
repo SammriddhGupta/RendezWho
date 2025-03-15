018aa811688d342510439f84f91cbe8dbf5a4319
Natalie Chen <z5592383@ad.unsw.edu.au>
2025-03-15 13:13:58 +1100

    Add calender select to home pg

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./index.css";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { isSameDay } from "date-fns";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 
 function Home() {
   const [eventName, setEventName] = useState("");
@@ -11,6 +14,31 @@ function Home() {
     alert(`Event "${eventName}" Created!`);
     navigate("/event"); // Navigate to the "/event" page
   };
+
+  const [value, setValue] = useState([]);
+
+  const handleDayClick = (day, modifiers) => {
+    const newValue = [...value];
+    if (modifiers.selected) {
+      const index = value.findIndex((d) => isSameDay(day, d));
+      newValue.splice(index, 1);
+    } else {
+      newValue.push(day);
+    }
+    setValue(newValue);
+  };
+
+  const handleResetClick = () => setValue([]);
+
+  let footer = <>Please pick one or more days.</>;
+
+  if (value.length > 0)
+    footer = (
+      <>
+        You selected {value.length} days.{" "}
+        <button onClick={handleResetClick}>Reset</button>
+      </>
+  );
 
   return (
     <>
@@ -27,6 +55,16 @@ function Home() {
               onChange={(e) => setEventName(e.target.value)}
               className="p-3 border rounded-lg w-full text-lg"
             />
+
+            <div>
+            <h1>Select Multiple Dates</h1>
+            <DayPicker
+              onDayClick={handleDayClick}
+              modifiers={{ selected: value }}
+              footer={footer}
+            />
+            </div>
+
             <button
               className="bg-purple-600 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-blue-700"
               onClick={handleCreateEvent} // Call handleCreateEvent function on click
